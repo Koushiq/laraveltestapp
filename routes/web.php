5962112f4f72d6f944fe40bea1b76c67bf8b2bd4
S.M Mushfiqur Rahman <koushiq1234@gmail.com>
2020-09-19 13:20:20 +0600

    Changes made

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/home/store','HomeController@store')->name('store');
+
+Route::get('/home/store/1','HomeController@custom');
